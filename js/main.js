52957da7e40be84189ac34f2c681b4529e6c9a4c
hideaking2021 <hide.freetimes.2021@gmail.com>
2022-04-14 14:52:32 +0900

    確率設定

--- js/main.js
+++ js/main.js
@@ -4,20 +4,33 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 3);
-    btn.textContent = n;
+    // const results = ['大吉','中吉','凶','末吉'];
+    // const results = ['大吉','大吉','大吉','大凶'];
+    // const n = Math.floor(Math.random() * results.length);
+    // btn.textContent = results[n];
+    // btn.textContent = results[Math.floor(Math.random() * results.length)];
 
-    switch (n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-        btn.textContent = '凶';
-        break;
-
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = '大吉';
+    } else if (n < 0.1) {
+      btn.textContent = '中吉';
+    }  else if (n < 0.3) {
+      btn.textContent = '末吉';
+    } else {
+      btn.textContent = '凶';
     }
+    // switch (n) {
+    //   case 0:
+    //     btn.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     btn.textContent = '中吉';
+    //     break;
+    //   case 2:
+    //     btn.textContent = '凶';
+    //     break;
+
+    // }
   });
 }
